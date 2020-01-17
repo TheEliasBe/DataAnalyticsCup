ac2ad2ed4ca87a7adfe64a3cb27b8b65a4cce008
Hermione21 <53211211+Hermione21@users.noreply.github.com>
2020-01-17 16:50:49 +0100

    Tried to join with ca_data

--- Main.py
+++ Main.py
@@ -6,25 +6,18 @@ from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import confusion_matrix
 
-# cant put a try catch block in lambda so here is a named function which does the trick
+# Can't put a try catch block in lambda so here is a named function which does the trick
 def save_division(a, b, c):
     try:
         return a / b
     except Exception as e:
         print(e)
         return c
-# Daten laden
-# TODO JOIN with CA_DATA
-cols = ["trip_start_timestamp", "trip_miles", "trip_seconds", "fare", "payment_type", "pickup_community_area", "dropoff_community_area", "pickup_centroid_latitude", "pickup_centroid_longitude", "dropoff_centroid_latitude", "dropoff_centroid_longitude"]
-df = pd.read_csv("train.csv", usecols=cols, delimiter=',')
-df = df.sample(n=10000) # Sample for better performance
 
-# remove stupid character froms datetieme string
-df['trip_start_timestamp'] = df['trip_start_timestamp'].apply(lambda x :( x.replace('T', ' ')).replace('Z',''))
 
-# is time during rush hour
+# Is time during rush hour?
 # TODO check for public holidays
-# easier to read rush hour checker than the lambda expression. DT has to be a datetime object
+# Easier to read rush hour checker than the lambda expression. DT has to be a datetime object
 def is_in_rush_hour(dt):
     if not isinstance(dt, datetime):
         raise Exception("is_in_rush_hour excepts a datetime object as input")
@@ -33,22 +26,48 @@ def is_in_rush_hour(dt):
         # saturday and sunday no rush hour
         return False
     else:
-        if datetime.strptime("07:00:00", "%H:%M:%S").time() <= dt.time() <= datetime.strptime("09:00:00", "%H:%M:%S").time():
+        if datetime.strptime("07:00:00", "%H:%M:%S").time() <= dt.time() <= datetime.strptime("09:00:00",
+                                                                                              "%H:%M:%S").time():
             return True
-        elif datetime.strptime("16:00:00", "%H:%M:%S").time() <= dt.time() <= datetime.strptime("18:00:00", "%H:%M:%S").time():
+        elif datetime.strptime("16:00:00", "%H:%M:%S").time() <= dt.time() <= datetime.strptime("18:00:00",
+                                                                                                "%H:%M:%S").time():
             return True
         else:
             return False
 
-df['in_rush_hour'] = df['trip_start_timestamp'].apply(lambda d : is_in_rush_hour(datetime.strptime(d, "%Y-%m-%d %H:%M:%S")))
-# now remove trip start timestamp
+
+# Load data
+# TODO JOIN with CA_DATA
+cols = ["trip_start_timestamp", "trip_miles", "trip_seconds", "fare", "payment_type", "pickup_community_area", "dropoff_community_area", "pickup_centroid_latitude", "pickup_centroid_longitude", "dropoff_centroid_latitude", "dropoff_centroid_longitude"]
+df = pd.read_csv("train.csv", usecols=cols, delimiter=',')
+df = df.sample(n=10000)  # Sample for better performance
+
+# Remove stupid character from datetime string
+df['trip_start_timestamp'] = df['trip_start_timestamp'].apply(lambda x: (x.replace('T', ' ')).replace('Z', ''))
+
+df['in_rush_hour'] = df['trip_start_timestamp'].apply(lambda d: is_in_rush_hour(datetime.strptime(d, "%Y-%m-%d %H:%M:%S")))
+
+# Now remove trip start timestamp
 df = df.drop(['trip_start_timestamp'], axis=1)
 
-# compute average speed of trip and remove all entries above 120mph
+# ____ CA_data ____ => as data types may be changed we have to take care of "trip_start_timestamp" before
+cols_ca = ["community_area_number", "community_area_name", "percent_of_housing_crowded", "percent_households_below_poverty", "percent_aged_over_15_unemployed", "percent_aged_over_24_without_high_school_diploma", "percent_aged_under_18_or_over_64", "per_capita_income", "hardship_index", "life_expectancy_1990", "life_expectancy_2000", "life_expectancy_2010", "predominant_non_english_language_percent", "avg_elec_usage_kwh", "avg_gas_usage_therms"]
+df_ca = pd.read_csv("CA_data.csv", usecols=cols_ca, delimiter=',')
+
+# Join df with CA_data on pickup_community_area and community_area_number
+df_joined = pd.merge(left=df, right=df_ca, left_on=["pickup_community_area"], right_on=["community_area_number"], how='outer', sort=False)
+print(list(df_joined))
+print(df_joined[["community_area_number", "pickup_community_area", "trip_start_timestamp"]])
+
+df = df_joined  # this line can easily be made into a comment to test only the original data set
+# ____ CA_data ____
+
+
+# Compute average speed of trip and remove all entries above 120mph
 df['avg_speed'] = df.apply(lambda row: save_division(row['trip_miles'], (row['trip_seconds']+1), 0)*3600, axis=1)
 df = df.drop(df[df['avg_speed']> 120].index)
 
-# compute fare per mile - maybe in rush hour 1 mile is more expensive, drop every fare >1000$
+# Compute fare per mile - maybe in rush hour 1 mile is more expensive, drop every fare >1000$
 # TODO normalize with trip_seconds
 df['fare_per_mile'] = df.apply(lambda row : save_division(row['fare'], row['trip_miles']+1, 0), axis=1)
 # df = df.drop(df[df['fare_per_mile'] > 1000.0].index)
@@ -56,7 +75,7 @@ df['fare_per_mile'] = df.apply(lambda row : save_division(row['fare'], row['trip
 
 
 
-# all NA community areas set to 0
+# All NA community areas set to 0
 df['pickup_community_area'] = df['pickup_community_area'].fillna(0)
 df['dropoff_community_area'] = df['dropoff_community_area'].fillna(0)
 df['pickup_centroid_latitude'] = df['pickup_centroid_latitude'].fillna(0)
@@ -64,8 +83,8 @@ df['pickup_centroid_longitude'] = df['pickup_centroid_longitude'].fillna(0)
 df['dropoff_centroid_latitude'] = df['dropoff_centroid_latitude'].fillna(0)
 df['dropoff_centroid_longitude'] = df['dropoff_centroid_longitude'].fillna(0)
 
-# move target value in_rush_hour to the back just for consistency
-df = df[['trip_seconds', 'trip_miles', 'pickup_community_area', 'dropoff_community_area', 'fare', 'payment_type', "pickup_centroid_latitude", "pickup_centroid_longitude", "dropoff_centroid_latitude", "dropoff_centroid_longitude", 'avg_speed', 'fare_per_mile', 'in_rush_hour']]
+# Move target value in_rush_hour to the back just for consistency
+df = df[['trip_seconds', 'trip_miles', 'pickup_community_area', 'dropoff_community_area', 'fare', 'payment_type', "pickup_centroid_latitude", "pickup_centroid_longitude", "dropoff_centroid_latitude", "dropoff_centroid_longitude", 'avg_speed', 'fare_per_mile', "community_area_number", "community_area_name", "percent_of_housing_crowded", "percent_households_below_poverty", "percent_aged_over_15_unemployed", "percent_aged_over_24_without_high_school_diploma", "percent_aged_under_18_or_over_64", "per_capita_income", "hardship_index", "life_expectancy_1990", "life_expectancy_2000", "life_expectancy_2010", "predominant_non_english_language_percent", "avg_elec_usage_kwh", "avg_gas_usage_therms",'in_rush_hour']]
 
 
 print("Size before dropping all NA values: ", df.shape)
@@ -74,9 +93,10 @@ print("Size after dropping all NA values: ", df.shape)
 # split dataset
 x1 = df.loc[df['in_rush_hour'] == True]
 x2 = df.loc[df['in_rush_hour'] == False]
+print(x1)
+print(x2)
 
-
-# avg speed ist schon aussagekräftig
+# Avg speed is actually significant
 def plt_avg_speed():
     fig = plt.figure()
     ax = fig.add_subplot()
@@ -87,7 +107,7 @@ def plt_avg_speed():
     ax.hist(x2['avg_speed'], bins=range(50), alpha=0.25, color='r')
     plt.show()
 
-# fare ist nicht sehr unterschiedlich
+# Fare doesn't differ much
 def plt_fare():
     fig = plt.figure()
     ax = fig.add_subplot()
@@ -124,13 +144,13 @@ def plt_fare_per_mile():
     plt.title("Fare Per Mile")
     plt.show()
 
-# problem: die farben überdecken sich, daher schwer per Auge einzuschätzen was in welchem Bereich überwieht
+# Problem: the dots may cover other coloured dots; hard to identify with own eyes which colour predominates an area
 def plt_pickup_coordinates():
     fig = plt.figure()
     ax = fig.add_subplot();
     ax.set_title("Geographic Coordinates")
     ax.scatter(x2['pickup_centroid_longitude'], x2['pickup_centroid_latitude'], c='b', s=3, alpha=0.5, label="No Rush Hour")
-    ax.scatter(x1['pickup_centroid_longitude'], x1['pickup_centroid_latitude'], c='r', s=3, alpha=0.5,label="Rush Hour")
+    ax.scatter(x1['pickup_centroid_longitude'], x1['pickup_centroid_latitude'], c='r', s=3, alpha=0.5, label="Rush Hour")
     plt.ylim(41.80,42.05)
     plt.xlim(-87.70, -87.55)
     plt.legend(loc='upper right')
@@ -152,12 +172,12 @@ def plt_pickup_coordinates2():
 # plt_payment_type()
 # plt_fare()
 # plt_payment_type()
-plt_pickup_community()
+# plt_pickup_community()
 # plt_dropoff_community()
 # plt_avg_speed()
 # plt_fare_per_mile()
-plt_pickup_coordinates()
-plt_pickup_coordinates2()
+# plt_pickup_coordinates()
+# plt_pickup_coordinates2()
 
 # print("CORRELATION TO IN_RUSH_HOUR BOOLEAN")
 print(df.corr(method='pearson')['in_rush_hour'])
