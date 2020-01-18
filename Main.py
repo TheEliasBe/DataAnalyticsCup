185307800106652043afbfb9523d7e8e695a6ff3
Elias <elias.berger@tum.de>
2020-01-19 00:41:14 +0100

    Correlation fixed, discretization

--- Main.py
+++ Main.py
@@ -26,11 +26,9 @@ def is_in_rush_hour(dt):
         # saturday and sunday no rush hour
         return False
     else:
-        if datetime.strptime("07:00:00", "%H:%M:%S").time() <= dt.time() <= datetime.strptime("09:00:00",
-                                                                                              "%H:%M:%S").time():
+        if datetime.strptime("07:00:00", "%H:%M:%S").time() <= dt.time() <= datetime.strptime("09:00:00","%H:%M:%S").time():
             return True
-        elif datetime.strptime("16:00:00", "%H:%M:%S").time() <= dt.time() <= datetime.strptime("18:00:00",
-                                                                                                "%H:%M:%S").time():
+        elif datetime.strptime("16:00:00", "%H:%M:%S").time() <= dt.time() <= datetime.strptime("18:00:00","%H:%M:%S").time():
             return True
         else:
             return False
@@ -40,7 +38,7 @@ def is_in_rush_hour(dt):
 # TODO JOIN with CA_DATA
 cols = ["trip_start_timestamp", "trip_miles", "trip_seconds", "fare", "payment_type", "pickup_community_area", "dropoff_community_area", "pickup_centroid_latitude", "pickup_centroid_longitude", "dropoff_centroid_latitude", "dropoff_centroid_longitude"]
 df = pd.read_csv("train.csv", usecols=cols, delimiter=',')
-df = df.sample(n=10000)  # Sample for better performance
+df = df.sample(n=150000)  # Sample for better performance
 
 # Remove stupid character from datetime string
 df['trip_start_timestamp'] = df['trip_start_timestamp'].apply(lambda x: (x.replace('T', ' ')).replace('Z', ''))
@@ -55,11 +53,9 @@ cols_ca = ["community_area_number", "community_area_name", "percent_of_housing_c
 df_ca = pd.read_csv("CA_data.csv", usecols=cols_ca, delimiter=',')
 
 # Join df with CA_data on pickup_community_area and community_area_number
-df_joined = pd.merge(left=df, right=df_ca, left_on=["pickup_community_area"], right_on=["community_area_number"], how='outer', sort=False)
-print(list(df_joined))
-print(df_joined[["community_area_number", "pickup_community_area", "trip_start_timestamp"]])
+df = pd.merge(left=df, right=df_ca, left_on=["pickup_community_area"], right_on=["community_area_number"], how='outer', sort=False)
 
-df = df_joined  # this line can easily be made into a comment to test only the original data set
+# df = df_joined  # this line can easily be made into a comment to test only the original data set
 # ____ CA_data ____
 
 
@@ -84,17 +80,24 @@ df['dropoff_centroid_latitude'] = df['dropoff_centroid_latitude'].fillna(0)
 df['dropoff_centroid_longitude'] = df['dropoff_centroid_longitude'].fillna(0)
 
 # Move target value in_rush_hour to the back just for consistency
-df = df[['trip_seconds', 'trip_miles', 'pickup_community_area', 'dropoff_community_area', 'fare', 'payment_type', "pickup_centroid_latitude", "pickup_centroid_longitude", "dropoff_centroid_latitude", "dropoff_centroid_longitude", 'avg_speed', 'fare_per_mile', "community_area_number", "community_area_name", "percent_of_housing_crowded", "percent_households_below_poverty", "percent_aged_over_15_unemployed", "percent_aged_over_24_without_high_school_diploma", "percent_aged_under_18_or_over_64", "per_capita_income", "hardship_index", "life_expectancy_1990", "life_expectancy_2000", "life_expectancy_2010", "predominant_non_english_language_percent", "avg_elec_usage_kwh", "avg_gas_usage_therms",'in_rush_hour']]
+df = df[['trip_seconds', 'trip_miles', 'pickup_community_area', 'dropoff_community_area', 'fare', 'payment_type', "pickup_centroid_latitude", "pickup_centroid_longitude", "dropoff_centroid_latitude", "dropoff_centroid_longitude", 'avg_speed', 'fare_per_mile', "community_area_number", "percent_of_housing_crowded", "percent_households_below_poverty", "percent_aged_over_15_unemployed", "percent_aged_over_24_without_high_school_diploma", "percent_aged_under_18_or_over_64", "per_capita_income", "hardship_index", "life_expectancy_1990", "life_expectancy_2000", "life_expectancy_2010", "avg_elec_usage_kwh", "avg_gas_usage_therms", "in_rush_hour"]]
 
 
 print("Size before dropping all NA values: ", df.shape)
 df = df.dropna()
 print("Size after dropping all NA values: ", df.shape)
+
+# manually set type necessary for some values. Discretization: convert float to int for some values
+df = df.astype({'in_rush_hour': 'bool', 'payment_type': 'str', 'avg_gas_usage_therms': 'int32', "avg_elec_usage_kwh": 'int32', 'trip_seconds': 'int32'})
+
+# print("CORRELATION TO IN_RUSH_HOUR BOOLEAN")
+corr = df.corr(method='pearson')['in_rush_hour'].apply(lambda x : abs(x))
+print(corr[corr>0.03])
+# TODO remove all attributes with |correlation| < 0.03 programmatically
+df = df[['trip_seconds', 'trip_miles', 'fare', 'pickup_centroid_latitude', 'pickup_centroid_longitude', 'avg_speed', 'percent_households_below_poverty', 'percent_aged_over_15_unemployed', 'percent_aged_under_18_or_over_64', 'life_expectancy_1990', 'life_expectancy_2000', 'life_expectancy_2010', 'fare_per_mile', 'payment_type', 'in_rush_hour']]
 # split dataset
 x1 = df.loc[df['in_rush_hour'] == True]
 x2 = df.loc[df['in_rush_hour'] == False]
-print(x1)
-print(x2)
 
 # Avg speed is actually significant
 def plt_avg_speed():
@@ -178,36 +181,33 @@ def plt_pickup_coordinates2():
 # plt_fare_per_mile()
 # plt_pickup_coordinates()
 # plt_pickup_coordinates2()
+#
+# normalize the data. some classifier work better on normalized data
+sc = StandardScaler()
+df[['trip_seconds', 'trip_miles', 'fare', 'avg_speed', 'fare_per_mile']] = sc.fit_transform(df[['trip_seconds', 'trip_miles', 'fare', 'avg_speed', 'fare_per_mile']])
 
-# print("CORRELATION TO IN_RUSH_HOUR BOOLEAN")
-print(df.corr(method='pearson')['in_rush_hour'])
-#
-# # normalize the data. some classifier work better on normalized data
-# sc = StandardScaler()
-# df[['trip_seconds', 'trip_miles', 'fare', 'avg_speed', 'fare_per_mile']] = sc.fit_transform(df[['trip_seconds', 'trip_miles', 'fare', 'avg_speed', 'fare_per_mile']])
-#
-# # encode the payment type as integer
-# le = LabelEncoder()
-# df['payment_type'] = le.fit_transform(df['payment_type'])
-#
-#
-# # split the data into train and test date
-# # TODO k-fold cross validation
-# training_set, validation_set = train_test_split(df, test_size = 0.2, random_state = 21)
-# X_train = training_set.iloc[:,0:-1].values
-# Y_train = training_set.iloc[:,-1].values
-# X_val = validation_set.iloc[:,0:-1].values
-# y_val = validation_set.iloc[:,-1].values
-#
-# # define a measure for accuracy
-# def accuracy(confusion_matrix):
-#    diagonal_sum = confusion_matrix.trace()
-#    sum_of_all_elements = confusion_matrix.sum()
-#    return diagonal_sum / sum_of_all_elements
-#
-# # create the neural network classifier
-# classifier = MLPClassifier(hidden_layer_sizes=(150,100,50), max_iter=300, activation = 'relu', solver='adam', random_state=1)
-# classifier.fit(X_train, Y_train)
-# y_pred = classifier.predict(X_val)
-# cm = confusion_matrix(y_pred, y_val)
-# print("Accuracy of MLPClassifier : ", accuracy(cm))
+# encode the payment type as integer
+le = LabelEncoder()
+df['payment_type'] = le.fit_transform(df['payment_type'])
+
+
+# split the data into train and test date
+# TODO k-fold cross validation
+training_set, validation_set = train_test_split(df, test_size = 0.2, random_state = 21)
+X_train = training_set.iloc[:,0:-1].values
+Y_train = training_set.iloc[:,-1].values
+X_val = validation_set.iloc[:,0:-1].values
+y_val = validation_set.iloc[:,-1].values
+
+# define a measure for accuracy
+def accuracy(confusion_matrix):
+   diagonal_sum = confusion_matrix.trace()
+   sum_of_all_elements = confusion_matrix.sum()
+   return diagonal_sum / sum_of_all_elements
+
+# create the neural network classifier
+classifier = MLPClassifier(hidden_layer_sizes=(11,100,100,2), max_iter=300, activation = 'relu', solver='adam', random_state=1)
+classifier.fit(X_train, Y_train)
+y_pred = classifier.predict(X_val)
+cm = confusion_matrix(y_pred, y_val)
+print("Accuracy of MLPClassifier : ", accuracy(cm))
